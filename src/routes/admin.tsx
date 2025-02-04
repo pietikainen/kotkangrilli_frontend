162b46052a60a23f2f2b338eba75c3dca05765d6
Lartza <Lartza@users.noreply.github.com>
2025-02-04 17:12:49 +0200

    Restore forgotten admin check

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,5 +1,7 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import React from "react";
+import { Center, Loader } from "@mantine/core";
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import React, { useEffect } from "react";
+import useGetUser from "../api/useGetUser.hook";
 import { Layout } from "../components/Layout";
 
 export const Route = createFileRoute("/admin")({
@@ -7,6 +9,27 @@ export const Route = createFileRoute("/admin")({
 });
 
 function RouteComponent() {
+  const { data: user, isError, isPending } = useGetUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isPending) {
+      if (isError || !user) {
+        navigate({ to: "/" });
+      } else if (user.data.userlevel < 8) {
+        navigate({ to: "/dashboard" });
+      }
+    }
+  }, [isPending, isError, user]);
+
+  if (isPending || !user || user.data.userlevel < 8) {
+    return (
+      <Center h="100vh">
+        <Loader />
+      </Center>
+    );
+  }
+
   return (
     <Layout>
       <Outlet />
